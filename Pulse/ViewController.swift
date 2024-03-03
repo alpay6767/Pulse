03beba93f06bf8b9ce822b35d0a3d1c0a756a37e
Alpay Kücük <akuecuek@itemis.com>
2024-03-03 15:10:30 +0100

    init commit

---
+++ Pulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Alpay Kücük on 02.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
